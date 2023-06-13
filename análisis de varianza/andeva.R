cf3397654d50067291b814f208da7f80d8746cd2
ByronG <byrong.gt@gmail.com>
2023-06-13 15:27:34 -0600

    actualizado Anova para infartos

--- análisis de varianza/andeva.R
+++ análisis de varianza/andeva.R
@@ -5,10 +5,48 @@ if(!require(readxl)){install.packages("readxl")}
 if(!require(flextable)){install.packages("flextable")}
 if(!require(tidyverse)){install.packages("tidyverse")}
 if(!require(dplyr)){install.packages("dplyr")}
-if(!require(rempsyc)){install.packages("rempsyc")}
+if(!require(performance)){install.packages("performance")}
 if(!require(ggplot2)){install.packages("ggplot2")}
 if(!require(ggstatsplot)){install.packages("ggstatsplot")}
+if(!require(agricolae)){install.packages("agricolae")}
 
 # Abrir la tabla de datos "centrosalud" -----
-centro<-read_excel("centrosalud.xlsx")
-head(centro)
+infarto<-read_excel("infarto.xlsx")
+head(infarto)
+tail(infarto)
+
+# Gráfico comparativo
+boxplot(infarto$total~infarto$grupo
+        , xlab = "Grupo"
+        , ylab = "Autoeficacias"
+        , col = "yellow")
+attach(infarto)
+model<-aov(total~grupo, data=infarto); model
+summary(model)
+infarto$predichos<-model$fitted.values
+infarto$residuos<-model$residuals
+head(infarto)
+
+model1<-lm(total~grupo, data = infarto)
+summary(model1)
+
+# Verificación de los supuestos del Anova
+
+boxplot(model$residuals~infarto$grupo, col="orange")
+
+# Hipótesis para la homogeneidad de varianzas de los residuos
+# Ho: Las varianzas son homogéneas 
+# Ha: Las varianzas son heterogéneas
+
+bartlett.test(total~grupo) # las varianza de los residuos son homogéneas
+
+windows(10,10)
+check_model(model1) # Ver el gráfico de residuos estandarizados vs predichos
+check_normality(model1)
+
+
+# Comparación gráfica
+ggbetweenstats(infarto, x=grupo, y=total, 
+               var.equal = T, plot.type = "box", type = "parametric")
+pr.medias <-LSD.test(model, "grupo",console=TRUE)
+
