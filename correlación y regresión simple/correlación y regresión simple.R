93fded9c07e549034eb8f9d0138adc464c68e6d8
ByronG <byrong.gt@gmail.com>
2023-06-13 15:37:50 -0600

    Actualizada correlación

--- correlación y regresión simple/correlación y regresión simple.R
+++ correlación y regresión simple/correlación y regresión simple.R
@@ -1,14 +1,40 @@
 # Dr. Byron González
 # http://www.byrong.tk 
 
+if(!require(RcmdrMisc)){install.packages("RcmdrMisc")}
+if(!require(Hmisc)){install.packages("Hmisc")}
+if(!require(corrplot)){install.packages("corrplot")}
+if(!require(ggcorrplot)){install.packages("ggcorrplot")}
 if(!require(readxl)){install.packages("readxl")}
-if(!require(flextable)){install.packages("flextable")}
-if(!require(tidyverse)){install.packages("tidyverse")}
-if(!require(dplyr)){install.packages("dplyr")}
-if(!require(rempsyc)){install.packages("rempsyc")}
-if(!require(ggplot2)){install.packages("ggplot2")}
-if(!require(ggstatsplot)){install.packages("ggstatsplot")}
 
-# Abrir la tabla de datos "centrosalud" -----
-centro<-read_excel("centrosalud.xlsx")
-head(centro)
+# Abrir la tabla de datos "centrosalud1" -----
+centro1<-read_excel("centrosalud1.xlsx")
+head(centro1)
+
+str(centro1)
+pairs(centro1)
+
+# Matriz de correlaciones
+names(centro1)
+cor(centro1[,c("edad","peso","talla","imc","pas","pad","fc")], use="complete")
+rcorr.adjust(centro1[,c("edad","peso","talla","imc","pas","pad","fc")], type="pearson", use="complete")
+
+# Correlograma de tipo círculo
+corrplot(cor(centro1), method="circle")
+
+# Correlograma de tipo elipse
+corrplot(cor(salinidad), method="ellipse")
+
+# Correlograma de tipo número
+corrplot(cor(salinidad), method="number")
+
+# Correlograma de tipo matriz superior
+corrplot(cor(salinidad), type="upper")
+
+# Correlograma de tipo matriz inferior
+corrplot(cor(salinidad), type="lower")
+
+# Correlograma de tipo mapa de calor
+sal<-cor(salinidad)
+ggcorrplot(sal)
+ggcorrplot(sal, type="lower", lab=T)
